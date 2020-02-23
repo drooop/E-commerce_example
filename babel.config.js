7c541e6a74f9637e96aa8114c60e6da2ffd57d73
Drooop <dropofsino@gmail.com>
2020-02-24 00:46:38 +0800

    npx vue-cli-service build --modern

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
-    ['@vue/app', {
-      polyfills: [
-        'es6.promise',
-        'es6.symbol'
-      ]
-    }]
-
+    '@vue/cli-plugin-babel/preset'
+    // {
+    //   polyfills: [
+    //     'es6.promise',
+    //     'es6.symbol'
+    //   ]
+    // }
   ],
   plugins: [
     [
