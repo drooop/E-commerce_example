892d98c285ef14314eba1b150a5913004b3d663b
drooop <dropofsino@gmail.com>
2020-02-23 23:14:20 +0800

    改正错误, change webserver port from 80(used) to 8000

--- back-end/for_shop/app.py
+++ back-end/for_shop/app.py
@@ -262,4 +262,4 @@ def changeUserState(uid, stateType):
 
 if __name__ == '__main__':
     app.debug = False  # 设置调试模式，生产模式的时候要关掉debug
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
